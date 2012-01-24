3c9af64eecc4f0ef962b60a01c53b4623fce647f
bishalonline <bishal.karki@lut.fi>
2012-01-24 15:22:11 +0200

    Settings Page Camera capture and photo browser

--- Projects/GEETHREE/GEETHREE/MainPage.xaml.cs
+++ Projects/GEETHREE/GEETHREE/MainPage.xaml.cs
@@ -10,11 +10,16 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+
 
 namespace GEETHREE
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        PhotoChooserTask photoChooserTask;
+        CameraCaptureTask cameraCaptureTask;
+
         // Constructor
         public MainPage()
         {
@@ -23,6 +28,15 @@ namespace GEETHREE
             // Set the data context of the listbox control to the sample data
             DataContext = App.ViewModel;
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+            // Photochoosertask : initializes the task object, and identifies the method to run after the user completes the task
+            photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += new EventHandler<PhotoResult>(photoChooserTask_Completed);
+
+            //Cameracapturetask : initializes the task object, and identifies the method to run after the user completes the task.
+            cameraCaptureTask = new CameraCaptureTask();
+            cameraCaptureTask.Completed += new EventHandler<PhotoResult>(cameraCaptureTask_Completed);
+
         }
 
         // Load data for the ViewModel Items
@@ -38,5 +52,62 @@ namespace GEETHREE
         {
             NavigationService.Navigate(new Uri("/SocietyPivot.xaml", UriKind.Relative));
         }
+
+        
+       
+
+        private void img_Settings_avatar_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            try
+            {
+                photoChooserTask.Show();
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                MessageBox.Show("An error occurred.");
+            }
+        }
+
+        private void img_Settings_camera_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            try
+            {
+                cameraCaptureTask.Show();
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                MessageBox.Show("An error occurred.");
+            }
+
+        }
+
+        //browses for the photos and gets the picture in imagebox after selection
+
+        void photoChooserTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+
+
+                //Code to display the photo on the page in an image control named myImage.
+                System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
+                bmp.SetSource(e.ChosenPhoto);
+                img_Settings_avatar.Source = bmp;
+            }
+        }
+
+        //Captures the picture using the camera and gets the picture in the imagebox 
+        void cameraCaptureTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+
+                //Code to display the photo on the page in an image control named myImage.
+                System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
+                bmp.SetSource(e.ChosenPhoto);
+                img_Settings_avatar.Source = bmp;
+            }
+        }
+
     }
 }
