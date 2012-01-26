a67cebfefc3911998d6cf1e3f695be306dc6e151
Thomster <tommi.kahkonen@lut.fi>
2012-01-26 16:39:22 +0200

    message canvas added

--- Projects/GEETHREE/GEETHREE/Pages/MessagesPage.xaml.cs
+++ Projects/GEETHREE/GEETHREE/Pages/MessagesPage.xaml.cs
@@ -10,15 +10,30 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using GEETHREE.DataClasses;
 
 namespace GEETHREE.Pages
 {
     public partial class MessagesPage : PhoneApplicationPage
     {
+        private Message selectedMessage = null;
         public MessagesPage()
         {
             InitializeComponent();
             DataContext = App.ViewModel;
+        }
+
+        private void MessagesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedMessage = (Message)MessagesListBox.SelectedItem;
+
+            messageCanvasMessageHeader.Text = selectedMessage.Header.ToString();
+            messageCanvasMessageContent.Text = selectedMessage.TextContent.ToString();
+            messageCanvas.Visibility = System.Windows.Visibility.Visible;
+            ApplicationBar.IsVisible = false;
+
+          
+        
         }
     }
 }
