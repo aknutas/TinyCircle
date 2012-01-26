c08300d8b056f3ad4ef214f743269fe0c4de7c7e
Thomster <tommi.kahkonen@lut.fi>
2012-01-26 12:43:47 +0200

    Camera and picture browsing functionality added also to message sending -page

--- Projects/GEETHREE/GEETHREE/Pages/ComposeMessagePage.xaml.cs
+++ Projects/GEETHREE/GEETHREE/Pages/ComposeMessagePage.xaml.cs
@@ -11,11 +11,15 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using GEETHREE.DataClasses;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace GEETHREE.Pages
 {
     public partial class ComposeMessagePage : PhoneApplicationPage
     {
+        PhotoChooserTask photoChooserTask;
+        CameraCaptureTask cameraCaptureTask;
         public ComposeMessagePage()
         {
             
@@ -28,6 +32,16 @@ namespace GEETHREE.Pages
                 listPicker1.Items.Add(u.UserName);
             
             }
+
+
+            // Photochoosertask : initializes the task object, and identifies the method to run after the user completes the task
+            photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += new EventHandler<PhotoResult>(photoChooserTask_Completed);
+
+            //Cameracapturetask : initializes the task object, and identifies the method to run after the user completes the task.
+            cameraCaptureTask = new CameraCaptureTask();
+            cameraCaptureTask.Completed += new EventHandler<PhotoResult>(cameraCaptureTask_Completed);
+
         }
 
         private void image1_Tap(object sender, System.Windows.Input.GestureEventArgs e)
@@ -46,5 +60,60 @@ namespace GEETHREE.Pages
             if (txt_compose_message.Text == "Type your message here...")
             txt_compose_message.Text = "";
         }
+
+        // start camera
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+
+            cameraCaptureTask.Show();
+   
+        }
+
+        // ** start picture browser
+        private void ApplicationBarIconButton_Click_1(object sender, EventArgs e)
+        {
+            photoChooserTask.Show();
+        }
+
+        //  send this message
+        private void ApplicationBarIconButton_Click_2(object sender, EventArgs e)
+        {
+            // ** ... communicate with networker to create a packet and send it ...
+            MessageBox.Show("Message sent.");
+        }
+
+        //browses for the photos and gets the picture in imagebox after selection
+        void photoChooserTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                // ...communication with the isolated storage...
+
+                BitmapImage bitImage = new BitmapImage();
+	            bitImage.SetSource(e.ChosenPhoto);
+                attachedImage.Source = bitImage;
+
+                //Write image to isolated storage
+                //ctrl.changeCurrentAvatar(e.ChosenPhoto);
+            }
+        }
+
+        //Captures the picture using the camera and gets the picture in the imagebox 
+        void cameraCaptureTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                // ...communication with the isolated storage...
+
+                BitmapImage bitImage = new BitmapImage();
+                bitImage.SetSource(e.ChosenPhoto);
+                attachedImage.Source = bitImage;
+
+                //Write image to isolated storage
+                //ctrl.changeCurrentAvatar(e.ChosenPhoto);
+            }
+        }
+
+
     }
 }
