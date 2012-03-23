e94cbfea85725db1dc49fdce00dc5ef612899613
Antti Knutas <antti.knutas@lut.fi>
2012-03-23 12:09:55 +0200

    Interface for receiving async web service calls
    Former-commit-id: c8316a576866935c2016eb039631c1cc37ae2151

---
+++ Projects/GEETHREE/GEETHREE/Networking/WebServiceReceiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GEETHREE.Networking
+{
+    interface WebServiceReceiver
+    {
+        void webServiceMessageEvent(List<DataClasses.Message> msgList);
+
+        void webServiceMessageSent(Boolean status);
+    }
+}
