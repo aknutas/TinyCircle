15fd3ebe63902cd127a527c9c198e652935ddb7f
tjkallonen <tommi.kallonen@gmail.com>
2012-03-30 11:46:28 +0300

    Fixed the crash when no network connection (check for error before accessing the results of web service calls)
    Former-commit-id: 29f4b5160d38c5fb408c062a1a664477c3494e5d

--- Projects/GEETHREE/GEETHREE/Networking/WebServiceConnector.cs
+++ Projects/GEETHREE/GEETHREE/Networking/WebServiceConnector.cs
@@ -68,58 +68,100 @@ namespace GEETHREE.Networking
             {
                 EventHandler<MsgServiceReference.getMyMessagesCompletedEventArgs> eh = new EventHandler<MsgServiceReference.getMyMessagesCompletedEventArgs>(msgService_getMyMessagesCompleted);
                 msgService.getMyMessagesCompleted += eh;
-                msgService.getMyMessagesAsync(userId, appKey);
+                try
+                {
+                    msgService.getMyMessagesAsync(userId, appKey);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message.ToString());
+                }
             }
 
             public void handlePostMessage(string userId, string recipient, string messageText)
             {
                 msgService.postMessageCompleted += new EventHandler<postMessageCompletedEventArgs>(msgService_postMessageCompleted);
+                try
+                {
                 msgService.postMessageAsync(recipient, userId, messageText, appKey);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message.ToString());
+                }
             }
 
             private void msgService_postMessageCompleted(object sender, postMessageCompletedEventArgs e)
             {
-                if (e.Result == true)
-                    wr.webServiceMessageSent(true);
+                if(e.Error==null)
+                {
+                    if (e.Result == true)
+                        wr.webServiceMessageSent(true);
+                    else
+                        wr.webServiceMessageSent(false);
+                }
                 else
-                    wr.webServiceMessageSent(false);
+                {
+                    System.Diagnostics.Debug.WriteLine(e.Error.Message.ToString());
+                }
             }
 
             private void msgService_getMyMessagesCompleted(object sender, MsgServiceReference.getMyMessagesCompletedEventArgs e)
             {
-                List<Message> returnMsgs = new List<Message>();
-                foreach (WireMessage wmsg in e.Result)
+                if(e.Error==null)
                 {
-                    Message msg = new Message();
-                    msg.ReceiverID = wmsg.recipientUserId;
-                    msg.SenderID = wmsg.senderUserId;
+                    List<Message> returnMsgs = new List<Message>();
+                    foreach (WireMessage wmsg in e.Result)
+                    {
+                        Message msg = new Message();
+                        msg.ReceiverID = wmsg.recipientUserId;
+                        msg.SenderID = wmsg.senderUserId;
                     
-                    msg.TextContent = wmsg.msgText;
-                    msg.PrivateMessage = true;
-                    returnMsgs.Add(msg);
-                }
+                        msg.TextContent = wmsg.msgText;
+                        msg.PrivateMessage = true;
+                        returnMsgs.Add(msg);
+                    }
 
-                wr.webServiceMessageEvent(returnMsgs);
+                    wr.webServiceMessageEvent(returnMsgs);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine(e.Error.Message.ToString());
+                }
             }
 
             public void handleTestConnection(WebServiceConnector webServiceConnector, WebServiceReceiver wr)
             {
                 parent = webServiceConnector;
                 msgService.pingCompleted += new EventHandler<pingCompletedEventArgs>(msgService_pingCompleted);
-                msgService.pingAsync(appKey);
+                try
+                {
+                    msgService.pingAsync(appKey);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message.ToString());
+                }
             }
 
             private void msgService_pingCompleted(object sender, pingCompletedEventArgs e)
             {
-                if (e.Result == true)
+                if (e.Error == null)
                 {
-                    parent.connectionUp = e.Result;
-                    wr.pingFinished(e.Result);
+                    if (e.Result == true)
+                    {
+                        parent.connectionUp = e.Result;
+                        wr.pingFinished(e.Result);
+                    }
+                    else
+                    {
+                        parent.connectionUp = false;
+                        wr.pingFinished(false);
+                    }
                 }
                 else
                 {
-                    parent.connectionUp = false;
-                    wr.pingFinished(false);
+                    System.Diagnostics.Debug.WriteLine(e.Error.Message.ToString());
                 }
             }
         }
