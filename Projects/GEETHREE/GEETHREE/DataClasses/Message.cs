2f2811b5243b6dab57d242a795f3fed23272577f
Antti Knutas <antti.knutas@lut.fi>
2012-04-19 18:15:32 +0300

    Bugfix: Nullable datetimes
    Former-commit-id: 7afb922eb0ddbba08f78a349befe8f10b4e4411b

--- Projects/GEETHREE/GEETHREE/DataClasses/Message.cs
+++ Projects/GEETHREE/GEETHREE/DataClasses/Message.cs
@@ -70,13 +70,13 @@ namespace GEETHREE.DataClasses
             }
         }
 
-        private DateTime _timestamp;
+        private DateTime? _timestamp;
         /// <summary>
         /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
         /// </summary>
         /// <returns></returns>
         [Column]
-        public DateTime Timestamp
+        public DateTime? Timestamp
         {
             get
             {
