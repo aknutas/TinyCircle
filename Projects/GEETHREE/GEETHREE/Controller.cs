f896ea7f2cb1d24d64af1a87dcd62a9ab3b6ddd9
tjkallonen <tommi.kallonen@gmail.com>
2012-05-03 14:09:23 +0300

    Adding the new friend to friendlist (after server response).
    Former-commit-id: b2e3660a1edf0dcf7327f4a3d945dac34e29f58b

--- Projects/GEETHREE/GEETHREE/Controller.cs
+++ Projects/GEETHREE/GEETHREE/Controller.cs
@@ -243,7 +243,20 @@ namespace GEETHREE
         public void notifyViewAboutFriend(string uid, string alias)
         {
             //First save friend info
+            bool exists = false;
+            foreach(User usr in dm.getAllUsers())
+            {
+                if (usr.UserID == uid)
+                    exists = true;
 
+            }
+            if (!exists)
+            {
+                User u = new User();
+                u.UserID = uid;
+                u.UserName = alias;
+                dm.storeNewUser(u);
+            }
 
             //Then notify
             if (currentPageName == "main")
